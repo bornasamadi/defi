763f62434d06bbef109e84ba033e5a8b685ff72b
Borna Samadi <borna@agencywolfe.com>
2020-10-11 15:31:46 -0400

    Updates

---
+++ precache-manifest.209bc305e5b7f665857daa07f9eb6ab9.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab3ee0246efdca97a4de9a80b89c09e",
+    "url": "/defi/index.html"
+  },
+  {
+    "revision": "8d61a2d6ff2a34710922",
+    "url": "/defi/static/css/2.be9ef181.chunk.css"
+  },
+  {
+    "revision": "af8dfc84093cc499755e",
+    "url": "/defi/static/css/main.56f87bee.chunk.css"
+  },
+  {
+    "revision": "8d61a2d6ff2a34710922",
+    "url": "/defi/static/js/2.4a632fe0.chunk.js"
+  },
+  {
+    "revision": "f846d1d1b3d7417390c999258a851cf6",
+    "url": "/defi/static/js/2.4a632fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fa4da9f1967d0b819c",
+    "url": "/defi/static/js/3.5c88e3f4.chunk.js"
+  },
+  {
+    "revision": "ecbf1351100a023556f38a09dc632797",
+    "url": "/defi/static/js/3.5c88e3f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67b9ce5025c33dc2a04",
+    "url": "/defi/static/js/4.4c278f23.chunk.js"
+  },
+  {
+    "revision": "4ad2da4d1e08bf4c850d",
+    "url": "/defi/static/js/5.12c3550f.chunk.js"
+  },
+  {
+    "revision": "af8dfc84093cc499755e",
+    "url": "/defi/static/js/main.6e95430d.chunk.js"
+  },
+  {
+    "revision": "029c6601ed090a6b444d09258de6b8ae",
+    "url": "/defi/static/js/main.6e95430d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d84402e71d61a82951e",
+    "url": "/defi/static/js/runtime-main.db993c8a.js"
+  },
+  {
+    "revision": "b35a142d7a9bbaef9c797cc7f4715d2d",
+    "url": "/defi/static/media/AUD.b35a142d.svg"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/defi/static/media/BTC.d7876d48.svg"
+  },
+  {
+    "revision": "5d08ccab9059d0350d3777b7a708c3b8",
+    "url": "/defi/static/media/CAD.5d08ccab.svg"
+  },
+  {
+    "revision": "c561bca49e9c856205a6c730c91a6dbf",
+    "url": "/defi/static/media/CHF.c561bca4.svg"
+  },
+  {
+    "revision": "87d6c3511f48ad35dd20f39111fcf86a",
+    "url": "/defi/static/media/CNY.87d6c351.svg"
+  },
+  {
+    "revision": "6d1ffc6c10d40230c513fa7f8c9b7622",
+    "url": "/defi/static/media/ETH.6d1ffc6c.svg"
+  },
+  {
+    "revision": "c248841998c12fbd99cfce5d67baa9cf",
+    "url": "/defi/static/media/EUR.c2488419.svg"
+  },
+  {
+    "revision": "35d544eaaa4cf3c6355866280d53ba73",
+    "url": "/defi/static/media/Flaticon.35d544ea.eot"
+  },
+  {
+    "revision": "3e4331ee31764c999add7e0b048c4ba3",
+    "url": "/defi/static/media/Flaticon.3e4331ee.ttf"
+  },
+  {
+    "revision": "5be3e43c13c3eb021d15e6682d098d4c",
+    "url": "/defi/static/media/Flaticon.5be3e43c.woff"
+  },
+  {
+    "revision": "c1729513a8741b7b61bae040816e426f",
+    "url": "/defi/static/media/Flaticon.c1729513.svg"
+  },
+  {
+    "revision": "29586ff0f963f4d1fdfc182822b8b27a",
+    "url": "/defi/static/media/Flaticon2.29586ff0.eot"
+  },
+  {
+    "revision": "b242ac810bd8cccaa03abc2128b7c3c3",
+    "url": "/defi/static/media/Flaticon2.b242ac81.woff"
+  },
+  {
+    "revision": "e1e2b6e05bbfd279181c693555c61bad",
+    "url": "/defi/static/media/Flaticon2.e1e2b6e0.svg"
+  },
+  {
+    "revision": "eafcbac04cdb0a39fe38a36ebd786290",
+    "url": "/defi/static/media/Flaticon2.eafcbac0.ttf"
+  },
+  {
+    "revision": "db68882cbf4beac3f3ee3923f1a91b82",
+    "url": "/defi/static/media/GBP.db68882c.svg"
+  },
+  {
+    "revision": "b5c00b410e5a59bc5c433ebcf9c19550",
+    "url": "/defi/static/media/INR.b5c00b41.svg"
+  },
+  {
+    "revision": "f8dc0f044c962d058420eb35d2228f83",
+    "url": "/defi/static/media/JPY.f8dc0f04.svg"
+  },
+  {
+    "revision": "793c8ae22dd469b9274e4002768beabb",
+    "url": "/defi/static/media/KRW.793c8ae2.svg"
+  },
+  {
+    "revision": "2a5ff834d12f21b421748941d6057d04",
+    "url": "/defi/static/media/Ki.2a5ff834.ttf"
+  },
+  {
+    "revision": "31f3692ca10d2f6d0bc467adba4533f7",
+    "url": "/defi/static/media/Ki.31f3692c.eot"
+  },
+  {
+    "revision": "451fd5bee09962b6977544325ace633f",
+    "url": "/defi/static/media/Ki.451fd5be.svg"
+  },
+  {
+    "revision": "8f9a1a6cffbd1826644e12714ec106d0",
+    "url": "/defi/static/media/Ki.8f9a1a6c.woff"
+  },
+  {
+    "revision": "3f226a426906fafba58c9d8ff8cdd11d",
+    "url": "/defi/static/media/LINK.3f226a42.svg"
+  },
+  {
+    "revision": "3749fe955d3250ac93855f579fcea37d",
+    "url": "/defi/static/media/Layout-4-blocks.3749fe95.svg"
+  },
+  {
+    "revision": "4f6065d8ae0660a7e87b3ec0a840df3e",
+    "url": "/defi/static/media/NZD.4f6065d8.svg"
+  },
+  {
+    "revision": "562474efbaa31ee8de76df31c4bd2116",
+    "url": "/defi/static/media/RUB.562474ef.svg"
+  },
+  {
+    "revision": "deda081753f0861b1d8b17ea8e939df5",
+    "url": "/defi/static/media/SGD.deda0817.svg"
+  },
+  {
+    "revision": "936feee6e8061e6bf4006dce278384dd",
+    "url": "/defi/static/media/USD.936feee6.svg"
+  },
+  {
+    "revision": "904b44c2b22eb2d49f618396e6f2db1a",
+    "url": "/defi/static/media/blue-loader.904b44c2.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "/defi/static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/defi/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/defi/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/defi/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/defi/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/defi/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/defi/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/defi/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/defi/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/defi/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/defi/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/defi/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/defi/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/defi/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/defi/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/defi/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "844ac0d0fe59dc4d60a977e32456694f",
+    "url": "/defi/static/media/ledger.844ac0d0.svg"
+  },
+  {
+    "revision": "c879a582a94e772b8ed27295ae83145d",
+    "url": "/defi/static/media/metamask.c879a582.svg"
+  },
+  {
+    "revision": "60e5857089e98edd838074c264d6c951",
+    "url": "/defi/static/media/socicon.60e58570.eot"
+  },
+  {
+    "revision": "944f06f5f65ef84a3a36e6c1c2d4b7ad",
+    "url": "/defi/static/media/socicon.944f06f5.woff"
+  },
+  {
+    "revision": "9a64ef938f9e55a70a4defc6ac9790bf",
+    "url": "/defi/static/media/socicon.9a64ef93.ttf"
+  },
+  {
+    "revision": "a35b65744f557fab5424e99bb6d4e980",
+    "url": "/defi/static/media/socicon.a35b6574.svg"
+  },
+  {
+    "revision": "e12a83718eed35b2f29ff79a2c211310",
+    "url": "/defi/static/media/trezor.e12a8371.svg"
+  },
+  {
+    "revision": "82b1c467a507265ecaafe1004abf5049",
+    "url": "/defi/static/media/trustwallet-i.82b1c467.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/defi/static/media/walletconnect.37f2bc6e.svg"
+  }
+]);
---
+++ static/js/5.12c3550f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdefi_dashboard=this.webpackJsonpdefi_dashboard||[]).push([[5],{1779:function(s,a){}}]);
+//# sourceMappingURL=5.12c3550f.chunk.js.map
